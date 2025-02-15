e498618827cf4df3a520e79d44333eb8c681bb94
chetanyaag <chetanyaagrawal@gmail.com>
2025-02-15 17:49:59 +0530

    add build

---
+++ dist/data-source.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppDataSource = void 0;
+require("reflect-metadata");
+const typeorm_1 = require("typeorm");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+exports.AppDataSource = new typeorm_1.DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "password",
+    database: process.env.DB_NAME || "nestjs_db",
+    entities: ["src/entities/*.ts"],
+    migrations: ["src/migrations/*.ts"],
+    synchronize: false, // Use migrations instead of auto-syncing
+    logging: true,
+});
+// export const AppDataSource = new DataSource({
+//     type: 'postgres',
+//     host: process.env.DB_HOST || 'localhost',
+//     port: Number(process.env.DB_PORT) || 5432,
+//     username: process.env.DB_USER || 'postgres',
+//     password: process.env.DB_PASS || 'password',
+//     database: process.env.DB_NAME || 'mydb',
+//     entities: ["src/entities/*.ts"],
+//     migrations: ['src/migrations/*.ts'],
+//     synchronize: false, // Migrations will handle schema changes
+//       logging: true,
+//   });
---
+++ dist/migrations/1739615306095-MigrationName.js
@@ -0,0 +1,32 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MigrationName1739615306095 = void 0;
+class MigrationName1739615306095 {
+    constructor() {
+        this.name = 'MigrationName1739615306095';
+    }
+    up(queryRunner) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield queryRunner.query(`CREATE TABLE \`category\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`description\` varchar(255) NOT NULL, \`isActive\` tinyint NOT NULL DEFAULT 1, \`createdAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`updatedAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+            yield queryRunner.query(`CREATE TABLE \`product\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`price\` decimal(10,2) NOT NULL, \`stockQuantity\` int NOT NULL, \`createdAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`updatedAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, \`categoryId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+            yield queryRunner.query(`ALTER TABLE \`product\` ADD CONSTRAINT \`FK_ff0c0301a95e517153df97f6812\` FOREIGN KEY (\`categoryId\`) REFERENCES \`category\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        });
+    }
+    down(queryRunner) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield queryRunner.query(`ALTER TABLE \`product\` DROP FOREIGN KEY \`FK_ff0c0301a95e517153df97f6812\``);
+            yield queryRunner.query(`DROP TABLE \`product\``);
+            yield queryRunner.query(`DROP TABLE \`category\``);
+        });
+    }
+}
+exports.MigrationName1739615306095 = MigrationName1739615306095;
